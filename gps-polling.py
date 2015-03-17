f14922559e3026b5969c6db2a5f38f697e4084b3
Vitor Hugo Medeiros Sousa <vhsousa@student.dei.uc.pt>
2015-03-17 15:57:41 +0000

    Fixing bugs

--- gps-polling.py
+++ gps-polling.py
@@ -12,37 +12,36 @@ gpsd = None #seting the global variable
 os.system('clear') #clear the terminal (optional)
 
 if __name__ == '__main__':
-  gpsd = gps(host="10.3.1.240", port=8080) #starting the stream of info
+    gpsd = gps(host="10.3.1.240", port=8080) #starting the stream of info
 
-  try:
+
     while True:
-      #It may take a second or two to get good data
-      #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
-      gpsd.next()
-      os.system('clear')
+        try:
+              #It may take a second or two to get good data
+              #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
+              gpsd.next()
+              os.system('clear')
 
-      print
-      print ' GPS reading'
-      print '----------------------------------------'
-      print 'latitude    ' , gpsd.fix.latitude
-      print 'longitude   ' , gpsd.fix.longitude
-      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-      print 'altitude (m)' , gpsd.fix.altitude
-      print 'eps         ' , gpsd.fix.eps
-      print 'epx         ' , gpsd.fix.epx
-      print 'epv         ' , gpsd.fix.epv
-      print 'ept         ' , gpsd.fix.ept
-      print 'speed (m/s) ' , gpsd.fix.speed
-      print 'climb       ' , gpsd.fix.climb
-      print 'track       ' , gpsd.fix.track
-      print 'mode        ' , gpsd.fix.mode
-      print
-      print 'sats        ' , gpsd.satellites
+              print
+              print ' GPS reading'
+              print '----------------------------------------'
+              print 'latitude    ' , gpsd.fix.latitude
+              print 'longitude   ' , gpsd.fix.longitude
+              print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+              print 'altitude (m)' , gpsd.fix.altitude
+              print 'eps         ' , gpsd.fix.eps
+              print 'epx         ' , gpsd.fix.epx
+              print 'epv         ' , gpsd.fix.epv
+              print 'ept         ' , gpsd.fix.ept
+              print 'speed (m/s) ' , gpsd.fix.speed
+              print 'climb       ' , gpsd.fix.climb
+              print 'track       ' , gpsd.fix.track
+              print 'mode        ' , gpsd.fix.mode
+              print
+              print 'sats        ' , gpsd.satellites
 
-      time.sleep(1) #set to whatever
+              time.sleep(1) #set to whatever
 
-  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-    print "\nKilling Thread..."
-    gpsp.running = False
-    gpsp.join() # wait for the thread to finish what it's doing
-  print "Done.\nExiting."
+        except Exception, e:
+            print e
+            continue
