e5b0af6dc60d5cc77f293c2f90ff2616ba05c58f
Vitor Hugo Medeiros Sousa <vhsousa@student.dei.uc.pt>
2015-03-17 15:59:35 +0000

    Fixing bugs

--- gps-polling.py
+++ gps-polling.py
@@ -12,33 +12,33 @@ gpsd = None #seting the global variable
 os.system('clear') #clear the terminal (optional)
 
 if __name__ == '__main__':
-    gpsd = gps(host="10.3.1.240", port=8080) #starting the stream of info
-
+    gpsd = gps(host="10.3.1.240", port=8080, mode=WATCH_ENABLE) #starting the stream of info
 
     while True:
         try:
               #It may take a second or two to get good data
-              #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
-              gpsd.next()
-              os.system('clear')
 
-              print
-              print ' GPS reading'
-              print '----------------------------------------'
-              print 'latitude    ' , gpsd.fix.latitude
-              print 'longitude   ' , gpsd.fix.longitude
-              print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-              print 'altitude (m)' , gpsd.fix.altitude
-              print 'eps         ' , gpsd.fix.eps
-              print 'epx         ' , gpsd.fix.epx
-              print 'epv         ' , gpsd.fix.epv
-              print 'ept         ' , gpsd.fix.ept
-              print 'speed (m/s) ' , gpsd.fix.speed
-              print 'climb       ' , gpsd.fix.climb
-              print 'track       ' , gpsd.fix.track
-              print 'mode        ' , gpsd.fix.mode
-              print
-              print 'sats        ' , gpsd.satellites
+              gpsd.next()
+              print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
+              #os.system('clear')
+
+              # print
+              # print ' GPS reading'
+              # print '----------------------------------------'
+              # print 'latitude    ' , gpsd.fix.latitude
+              # print 'longitude   ' , gpsd.fix.longitude
+              # print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+              # print 'altitude (m)' , gpsd.fix.altitude
+              # print 'eps         ' , gpsd.fix.eps
+              # print 'epx         ' , gpsd.fix.epx
+              # print 'epv         ' , gpsd.fix.epv
+              # print 'ept         ' , gpsd.fix.ept
+              # print 'speed (m/s) ' , gpsd.fix.speed
+              # print 'climb       ' , gpsd.fix.climb
+              # print 'track       ' , gpsd.fix.track
+              # print 'mode        ' , gpsd.fix.mode
+              # print
+              # print 'sats        ' , gpsd.satellites
 
               time.sleep(1) #set to whatever
 
