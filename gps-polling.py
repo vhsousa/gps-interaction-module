88c50df188e9d7879301688c4a2114de1e0d737c
Vitor Hugo Medeiros Sousa <vhsousa@student.dei.uc.pt>
2015-03-17 16:00:27 +0000

    Fixing bugs

--- gps-polling.py
+++ gps-polling.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
               #It may take a second or two to get good data
 
               gpsd.next()
-              print gpsd
-              print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
+              print
+              print gpsd, gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
               #os.system('clear')
 
               # print
